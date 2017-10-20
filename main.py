e5f2d1a22b40d228068b2550582d28639e56e2cc
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-10-20 09:25:15 +0300

    Added todos

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from core.Benchmark import Benchmark
+from algorithms import *
 
 if __name__ == "__main__":
-    no_discs = raw_input("Number of discs: ")
-    no_rods = raw_input("Number of rods: ")
+    """TODO: implement custom initial state and final state
+             implement random optimized with custom backsteps (intermediate steps)
+             hillclimbing: consider current state score
+                           sum, prod, ponderat
+                           scadere scor doar o tranzitie
+             astar: from final state to initial state
+    """
 
-    benchmark = Benchmark(int(no_discs), int(no_rods))
-    benchmark.test()
+    # no_discs = raw_input("Number of discs: ")
+    # no_rods = raw_input("Number of rods: ")
+    #
+    # benchmark = Benchmark(int(no_discs), int(no_rods))
+    # benchmark.test()
+
+    star = AStarAlgorithm(3, 3)
+    star.solve()
+    print star.steps()
